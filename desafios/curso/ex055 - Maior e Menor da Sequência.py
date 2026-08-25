4e40e5646d816621a71eb0807b3f5b3518d17529
Wallyston <wallyston98@gmail.com>
2026-08-25 15:18:18 -0300

    feat: conclusão de duas atividades

--- desafios/curso/ex055 - Maior e Menor da Sequência.py
+++ desafios/curso/ex055 - Maior e Menor da Sequência.py
@@ -1,1 +1,23 @@
 # Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos
+
+# Entrada de dados
+maior = 0
+menor = 0
+
+# Entrada de dados e processamento
+for p in range(1,6):
+    peso = float(input('Digite o peso da {}º pessoa (Kg): '.format(p)))
+
+    # Manipulação para o maior e menor peso
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        elif peso < menor:
+            menor = peso
+
+# Saída de resultados
+print('O maior peso é o {}'.format(maior))
+print('O menor peso é o {}'.format(menor))
--- desafios/curso/ex056 - Analisador Completo.py
+++ desafios/curso/ex056 - Analisador Completo.py
@@ -1,1 +1,40 @@
 # Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: A média de idade do grupo. Qual é o nome do homem mais velho. Quantas mulheres tem menos de 20 anos.
+
+# Entrada de dados
+homemvelho = ''
+idadehomem = 0
+somaidade = 0
+mulhernova = 0
+
+# Entrada de dados e Processamento
+for c in range (1, 5):
+    nome = str(input('Digite o nome da {}º pessoa: '.format(c))).strip().title()
+    idade = int(input('Digite sua idade: '))
+    genero = str(input('Qual é o seu gênero? (M) Masculino. (F) Feminino: ')).upper().strip()
+
+    # Mulheres com menos de 20 anos.
+    if genero == 'F' and idade < 20:
+        mulhernova += 1
+
+    # Soma a idade para calcular a média
+    somaidade += idade
+
+    # Manipulação para identificar o homem mais velho
+    if genero == 'M':
+        if c == 1:
+            homemvelho = nome
+            idadehomem = idade
+        elif idade > idadehomem:
+            homemvelho = nome
+            idadehomem = idade
+
+# Média de idade do grupo
+media = somaidade / 4
+
+# Saída de resultados
+print('-'*35)
+print('A média de idade do grupo é {} anos'.format(media))
+print('O homem mais velho se chama {} e tem {} anos.'.format(homemvelho, idadehomem))
+print('No grupo tem {} mulheres com menos de 20 anos.'.format(mulhernova))
+print('-'*35)
+
