f896c6dfd5ecceff16668384ce161ab1b9696930
Wallyston <wallyston98@gmail.com>
2026-09-03 15:37:05 -0300

    Update ex050 - Soma dos pares.py

--- desafios/curso/ex050 - Soma dos pares.py
+++ desafios/curso/ex050 - Soma dos pares.py
@@ -1,11 +1,16 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for impar, desconsidere-o
 
-# Entrada de dados
+# Contadores
 soma = 0
+cont = 0
+
+# Entrada de dados e processamento
 for num in range(1, 7):
     int(input('Digite {}ª número: '.format(num)))
+    # Verificação
     if num % 2 == 0: 
         soma += num
+        cont += 1
 
 # Saida de resultados
-print('A soma total de todos os números pares é {}'.format(soma))
+print('A soma total de todos os {} números pares é {}'.format(cont, soma))
