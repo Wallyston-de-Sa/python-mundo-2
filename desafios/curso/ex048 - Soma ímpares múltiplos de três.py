6eb2e44bbd24dc2f7ff57017b3f61802912fbcce
Wallyston <wallyston98@gmail.com>
2026-09-02 23:04:14 -0300

    doc: correção

--- desafios/curso/ex048 - Soma ímpares múltiplos de três.py
+++ desafios/curso/ex048 - Soma ímpares múltiplos de três.py
@@ -1,9 +1,11 @@
 # Faça um programa que calcule a soma entre todos os números ímpares que são multiplos de três e que se encontram no intervalo de 1 até 500
-# Entrada de dados
+# Contagem
 soma = 0
+contador = 0
 
-# Saída de resultados
-for cont in range (1, 501):
-    if cont % 2 != 0 and cont % 3 == 0:
+# Entrada de dados e processamento
+for cont in range (1, 501, 2):
+    if cont % 3 == 0:
+        contador += 1
         soma += cont
-print('A soma entre todos os números ímpares que são multiplos de três entre 1 à 500 é {}'.format(soma))
+print('A soma entre todos os {} números ímpares que são multiplos de três entre 1 à 500 é {}'.format(contador, soma))
