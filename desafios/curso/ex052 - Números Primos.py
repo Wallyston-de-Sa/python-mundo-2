75743d937b06017fd56473573832859ef40c8d10
Wallyston <wallyston98@gmail.com>
2026-09-08 16:26:30 -0300

    Update ex052 - Números Primos.py

--- desafios/curso/ex052 - Números Primos.py
+++ desafios/curso/ex052 - Números Primos.py
@@ -1,5 +1,11 @@
 # Faça um programa que leia um número inteiro e diga se ele é ou não um número primo
 
+# Cores
+cores = {
+    'limpa': '\033[m',
+    'vermelho': '\033[31m',
+    'verde': '\033[32m' }
+
 # Contador
 cont = 0
 
@@ -7,8 +13,11 @@ cont = 0
 num = int(input('Qual número deseja saber se é primo? '))
 for c in range(1, num + 1):
     if num %  c == 0:
+        print('{}{}{}'.format(cores['verde'], c, cores['limpa']), end=' ')
         cont += 1
-    print(c, end=' ')
+    else:
+        print('{}{}{}'.format(cores['vermelho'], c, cores['limpa']), end=' ')
+
 
 # Saída de resultados
 print('\nO número {} foi divisível {}º vezes.'.format(num, cont))
