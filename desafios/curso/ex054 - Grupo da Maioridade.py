6b98ee4ca0334274cc6b353b9fb47d3a8187a5ec
Wallyston <wallyston98@gmail.com>
2026-09-09 11:15:51 -0300

    Update ex054 - Grupo da Maioridade.py

--- desafios/curso/ex054 - Grupo da Maioridade.py
+++ desafios/curso/ex054 - Grupo da Maioridade.py
@@ -1,14 +1,19 @@
 # Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores
 from datetime import date
+
+# Contadores
 menores = 0
 maiores = 0
 
-for c in range (7):
-    nascimento = int(input('Digite o ano do seu nascimento: '))
+# Entrada de dados e processamento
+for c in range (1, 8):
+    nascimento = int(input('Digite o ano em que a {}º pessoa nasceu: '.format(c)))
     idade = date.today().year - nascimento
     if idade > 18:
         maiores += 1
     else:
         menores += 1
+
+# Saída de resultados
 print('{} pessoas são menores de 18 anos.'.format(menores))
 print('{} pessoas são maiores de 18 anos.'.format(maiores))
